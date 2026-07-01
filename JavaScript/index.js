c385b63506319edcb28e6120862f36faafafebd1
rishabg01 <rishabg1000@gmail.com>
2026-07-01 11:56:04 +0530

    javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,33 @@
+// console.log("hello world"); 
+
+// let obj={
+//     ht:189,
+//     wt:64,
+//     name:"shagun",
+
+
+// }
+// console.log(obj);
+
+// // let des={...obj};
+// // console.log(des);
+// // obj.ht=90;
+// // console.log(obj);
+
+// let des=Object.assign({},obj);
+// console.log(des);
+
+// try{
+//     console.log(x);
+// }
+// catch(err){
+//     console.log("error");
+//     console.log(err);
+// }
+
+try{
+    console.log(x);
+}
+catch(err){
+  throw new Error("err");
+}
