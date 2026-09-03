de7cb64330d07a13e19c30cc1225d41dea1e0386
rishabg01 <rishabg1000@gmail.com>
2026-09-03 17:36:25 +0530

    js

---
+++ JavaScript/index3.js
@@ -0,0 +1,50 @@
+// let odd=function(num){
+//     console.log(!(num%2==0));   
+// }
+// let even=function(num){
+//     console.log(num%2==0);   
+// }
+
+// const  object = {
+//      name:"rishab",
+//      age:20,
+//      city:"delhi",
+//      getvalue(){
+//         console.log(`${this.name} is ${this.age} years old and lives in ${this.city}`);
+//      }
+
+// }
+
+//ARROW FUNCTION 
+// const func=(a,b)=>(a*b*3);
+
+// //settimeout
+
+// console.log("start");
+   
+
+
+// setTimeout(()=>{
+//     console.log("middle");
+// },4000);
+
+
+
+
+
+// console.log("end");
+    
+console.log("start");
+   
+
+
+let id1=setInterval(()=>{
+    console.log("middle");
+},4000);
+
+
+
+
+
+console.log("end");
+    
